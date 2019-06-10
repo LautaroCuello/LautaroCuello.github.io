f0eaae861eea21b5b9dd69170dfb35dce3845792
LautaroCuello <LautaroNCuello@gmail.com>
2019-06-10 01:50:26 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,26 +1,89 @@
-var puntaje_maximo = 52.50;
-var puntaje_resultante = 0.0;
-
-function comprobarAnios(valor){
-    if(Number(valor)){
-        valor = Number(valor)
-        if(valor % 1 == 0 && valor <= 10 && valor >= 0){
-            return true; // entero
-        } else {
-            return false; // fallo, flotante
+function obtainTextInput(valor){
+    var inputs = document.getElementById(valor).childNodes[1].getElementsByTagName("div");
+    var inputs_result = new Array();
+    for(var i = 0; i < inputs.length; i++){
+        aux = document.getElementById(valor).childNodes[1].getElementsByTagName("div")[i].getElementsByTagName("input");
+        if(aux.length == 1){
+            inputs_result.push(aux[0]);
         }
+    }
+    return inputs_result;
+}
+
+function obtainRadioInput(valor){
+    var inputs = document.getElementById(valor).childNodes[1].getElementsByTagName("div");
+    var inputs_result = new Array();
+    for(var i = 0; i < inputs.length; i++){
+        aux = document.getElementById(valor).childNodes[1].getElementsByTagName("div")[i].getElementsByTagName("input");
+        if(aux.length > 1){
+            inputs_result.push(aux);
+        }
+    }
+    return inputs_result;
+}
+
+function checkNumber(value){
+    if(Number(value) >= 0){
+        return true;
     } else {
-        return false; // fallo, no es un numero
+        return false;
     }
 }
 
-function comprobarRadios(direccion){
-    var radios = document.getElementsByName(direccion);
-    for(var i = 0; i < radios.length; i++){
-        if(radios[i].checked){
-            return radios[i].value;
+function checkLimit(value, limit){
+    if(Number(value) <= limit){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkInteger(value){
+    if(Number(value) % 2 == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkRadio(value){
+    for(var i = 0; i < value.length; i++){
+        if(value[i].checked){
+            return value[i].value;
         }
     }
+}
+
+function validateData(data, is_Number, is_Integer, limit, multiply, location){
+    if(is_Number){
+        if(checkNumber(data)){
+            if(is_Integer){
+                if(checkInteger(data)){
+                    if(checkLimit(data, limit)){
+                        return Number(data) * multiply;
+                    } else {
+                        return " ERROR EN " + location
+                    }
+                } else {
+                    return " ERROR EN " + location
+                }
+            } else {
+                if(checkLimit(data, limit)){
+                    return Number(data) * multiply;
+                } else {
+                    return " ERROR EN " + location
+                }
+            }
+        } else {
+            return " ER2ROR EN " + location
+        }
+    } else {
+        return Number(checkRadio(data));
+    }
+}
+
+function setResult(data){
+    document.getElementsByName("ttyav_resultado")[0].value = data;
 }
 
 function borrarCampo(valor){
@@ -30,116 +93,54 @@ function borrarCampo(valor){
 }
 
 function ttyav_obtenerResultado(){
-    if(puntaje_resultante > 0){
-        return NaN;
-    }
+    var ttyav_resultado = 0.00;
+    var text_inputs = obtainTextInput("ttyav");
+    var radio_inputs = obtainRadioInput("ttyav");
 
-    puntaje_resultante = 0.0;
+    //A
+    ttyav_resultado += validateData(text_inputs[0].value, true, false, 25.00, 1.00, "A");
+    console.log(ttyav_resultado);
 
-    var flag = true;
-    // A
-    flag = comprobarAnios(document.getElementsByName("ttyav_titulo")[0].value);
-    // SI ES ERROR
-    if(!flag){
-        document.getElementsByName("ttyav_resultado")[0].value = "ERROR";
-        return NaN;
-    }
-    var valor = document.getElementsByName("ttyav_titulo")[0].value;
-    puntaje_resultante += 1.0;
+    //B
+    ttyav_resultado += validateData(text_inputs[1].value, true, true, 10, 0.25, "B");
+    console.log(ttyav_resultado);
 
-    // B
-    flag = comprobarAnios(document.getElementsByName("ttyav_antiguedad_titulo")[0].value);
-    // SI ES ERROR
-    if(!flag){
-        document.getElementsByName("ttyav_resultado")[0].value = "ERROR";
-        return NaN;
-    }
-    var valor = document.getElementsByName("ttyav_antiguedad_titulo")[0].value;
-    puntaje_resultante += Number(valor) * 0.25;
+    //C
+    ttyav_resultado += validateData(radio_inputs[0], false, false, 0, 0, "C");
+    console.log(ttyav_resultado);
 
-    // C
-    valor = comprobarRadios("ttyav_promedio_titulo");
-    puntaje_resultante += Number(valor);
+    //D
+    ttyav_resultado += validateData(text_inputs[2].value, true, true, 10.00, 0.5, "D");
+    console.log(ttyav_resultado);
 
-    // D
-    flag = comprobarAnios(document.getElementsByName("ttyav_antiguedad_doc_edsup")[0].value);
-    // SI ES ERROR
-    if(!flag){
-        document.getElementsByName("ttyav_resultado")[0].value = "ERROR";
-        return NaN;
-    }
-    var valor = document.getElementsByName("ttyav_antiguedad_doc_edsup")[0].value;
-    puntaje_resultante += Number(valor) * 0.5;
+    //E
+    ttyav_resultado += validateData(text_inputs[3].value, true, true, 10.00, 0.5, "E");
+    console.log(ttyav_resultado);
 
-    // E
-    flag = comprobarAnios(document.getElementsByName("ttyav_antiguedad_doc_misma_area")[0].value);
-    // SI ES ERROR
-    if(!flag){
-        document.getElementsByName("ttyav_resultado")[0].value = "ERROR";
-        return NaN;
-    }
-    var valor = document.getElementsByName("ttyav_antiguedad_doc_misma_area")[0].value;
-    puntaje_resultante += Number(valor) * 0.5;
+    //F
+    ttyav_resultado += validateData(text_inputs[4].value, true, true, 10.00, 0.25, "F");
+    console.log(ttyav_resultado);
 
-    // F
-    flag = comprobarAnios(document.getElementsByName("ttyav_antiguedad_doc_desfav")[0].value);
-    // SI ES ERROR
-    if(!flag){
-        document.getElementsByName("ttyav_resultado")[0].value = "ERROR";
-        return NaN;
-    }
-    var valor = document.getElementsByName("ttyav_antiguedad_doc_desfav")[0].value;
-    puntaje_resultante += Number(valor) * 0.25;
+    //G
+    ttyav_resultado += validateData(text_inputs[5].value, true, true, 10.00, 0.25, "G");
+    console.log(ttyav_resultado);
 
-    // G
-    flag = comprobarAnios(document.getElementsByName("ttyav_antiguedad_doc_mismo_escalafon")[0].value);
-    // SI ES ERROR
-    if(!flag){
-        document.getElementsByName("ttyav_resultado")[0].value = "ERROR";
-        return NaN;
-    }
-    var valor = document.getElementsByName("ttyav_antiguedad_doc_mismo_escalafon")[0].value;
-    puntaje_resultante += Number(valor) * 0.25;
+    //H
+    ttyav_resultado += validateData(text_inputs[6].value, true, true, 10.00, 0.25, "H");
+    console.log(ttyav_resultado);
 
-    // H
-    flag = comprobarAnios(document.getElementsByName("ttyav_antiguedad_doc_perspectivas")[0].value);
-    // SI ES ERROR
-    if(!flag){
-        document.getElementsByName("ttyav_resultado")[0].value = "ERROR";
-        return NaN;
-    }
-    var valor = document.getElementsByName("ttyav_antiguedad_doc_perspectivas")[0].value;
-    puntaje_resultante += Number(valor) * 0.25;
+    //I
+    ttyav_resultado += validateData(text_inputs[7].value, true, true, 10.00, 0.1, "I");
+    console.log(ttyav_resultado);
 
-    // I
-    flag = comprobarAnios(document.getElementsByName("ttyav_antiguedad_doc_perspectivas_desfavorables")[0].value);
-    // SI ES ERROR
-    if(!flag){
-        document.getElementsByName("ttyav_resultado")[0].value = "ERROR";
-        return NaN;
-    }
-    var valor = document.getElementsByName("ttyav_antiguedad_doc_perspectivas_desfavorables")[0].value;
-    puntaje_resultante += Number(valor) * 0.1;
+    //J
+    ttyav_resultado += validateData(radio_inputs[1], false, false, 0, 0, "J");
+    console.log(ttyav_resultado);
 
-    // J
-    valor = comprobarRadios("ttyav_antiguedad_calif_ult");
-    puntaje_resultante += Number(valor);
+    //K
+    ttyav_resultado += validateData(text_inputs[8].value, true, false, 5.00, 1.00, "K");
+    console.log(ttyav_resultado);
 
-    // K
-    flag = comprobarAnios(document.getElementsByName("ttyav_antiguedad_otros_titulos")[0].value);
-    // SI ES ERROR
-    if(!flag){
-        document.getElementsByName("ttyav_resultado")[0].value = "ERROR";
-        return NaN;
-    }
-    var valor = document.getElementsByName("ttyav_antiguedad_otros_titulos")[0].value;
-    puntaje_resultante += 1.0;
+    setResult(ttyav_resultado);
 
-    //RESULTADO
-    if(puntaje_resultante > puntaje_maximo){
-        document.getElementsByName("ttyav_resultado")[0].value = "ERROR";
-        return NaN;
-    } else {
-        document.getElementsByName("ttyav_resultado")[0].value = puntaje_resultante.toFixed(2);
-    }
 }
